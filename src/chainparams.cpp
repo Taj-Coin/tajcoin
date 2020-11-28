53ed3b2a9070373685c487a385c2418c832ceb7c
Crypto.si <cryptosi@users.noreply.github.com>
2020-11-28 09:55:40 +0100

    Build: C++11

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -87,8 +87,8 @@ public:
         vSeeds.push_back(CDNSSeedData("tajcoin.tech", "node9.tajcoin.tech"));
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,65);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
-        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,111);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5)
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,111)
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
