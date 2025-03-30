931d7bf0be5fcec809485fe02a1df3b38f1902fc
ANEY <121462141+Animal-Economy@users.noreply.github.com>
2025-03-31 00:42:14 +0200

    Update version.h

--- src/version.h
+++ src/version.h
@@ -30,13 +30,13 @@ static const int DATABASE_VERSION = 70000;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60000;
+static const int PROTOCOL_VERSION = 60001;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 200;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 200;
+static const int MIN_PEER_PROTO_VERSION = 201;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
