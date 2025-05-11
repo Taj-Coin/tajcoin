9da0925ec370eefe7a8a87aa2df30d1a7060511c
Gregor Pogacnik <grega@pogacnik.net>
2025-05-11 19:17:53 +0200

    fix: build issues

--- src/rpcprotocol.h
+++ src/rpcprotocol.h
@@ -105,7 +105,11 @@ public:
     }
     bool connect(const std::string& server, const std::string& port)
     {
+#if BOOST_VERSION >= 107000
         boost::asio::ip::tcp::resolver resolver(stream.get_executor());
+#else
+        boost::asio::ip::tcp::resolver resolver(stream.lowest_layer().get_io_service());
+#endif
         boost::asio::ip::tcp::resolver::query query(server.c_str(), port.c_str());
         boost::asio::ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
         boost::asio::ip::tcp::resolver::iterator end;
